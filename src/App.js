94cfb993694f5c41e7372a470a1604c00f6f46b1
wldzimerz <kvkad13@bk.ru>
2021-09-10 19:16:57 +0700

    fix: logic and code improvement

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // import s from "./App.module.css";
-import Header from "./components/Header";
-import Layout from "./components/Layout";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
 import background from "./assets/bg3.jpg";
 
 function App() {
   return (
     <>
       <Header title="Pokemon game" descr="Simple Triple Triad Card Game" />
-      <Layout title="firstLayout title" descr="firstLayout descr" urlBg={background} colorBg="" />
-      <Layout title="secondLayout title" descr="secondLayout descr" urlBg="" colorBg="#e2e2e2" />
-      <Layout title="thirdLayout title" descr="thirdLayout descr" urlBg={background} colorBg="" />
+      <Layout title="firstLayout title" descr="firstLayout descr" urlBg={background} />
+      <Layout title="secondLayout title" descr="secondLayout descr" colorBg="#e2e2e2" />
+      <Layout title="thirdLayout title" descr="thirdLayout descr" urlBg={background} />
       <Footer />
     </>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
 import s from "./Layout.module.css";
 
-const Layout = (props) => {
-  const back = props.urlBg ? { backgroundImage: `url(${props.urlBg})` } : { backgroundColor: props.colorBg };
+const Layout = ({ title, descr, urlBg, colorBg = "#e2e2e2" }) => {
+  const back = urlBg && { backgroundImage: `url(${urlBg})` } ? { backgroundImage: `url(${urlBg})` } : { backgroundColor: colorBg };
   return (
     <section className={s.root} style={back}>
       <div className={s.wrapper}>
         <article>
           <div className={s.title}>
-            <h3>{props.title}</h3>
+            <h3>{title}</h3>
             <span className={s.separator}></span>
           </div>
           <div className={`{${s.desc}, ${s.full}}`}>
-            <p> {props.descr} </p>
+            <p> {descr} </p>
           </div>
         </article>
       </div>
