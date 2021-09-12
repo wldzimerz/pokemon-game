1f300e0285d0c26c779198778b7d55663e07159f
wldzimerz <kvkad13@bk.ru>
2021-09-12 15:54:19 +0700

    feat: cards realization (lesson-03)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-// import s from "./App.module.css";
+import "./App.css";
 import Header from "./components/Header/Header";
 import Layout from "./components/Layout/Layout";
 import Footer from "./components/Footer/Footer";
-import background from "./assets/bg3.jpg";
+import background1 from "./assets/bg3.jpg";
+import background2 from "./assets/bg1.jpg";
+import POKEMONS from "../src/data/pokemons.json";
+import PokemonCard from "./components/Pokemon Card/PokemonCard";
 
 function App() {
   return (
     <>
-      <Header title="Pokemon game" descr="Simple Triple Triad Card Game" />
-      <Layout title="firstLayout title" descr="firstLayout descr" urlBg={background} />
-      <Layout title="secondLayout title" descr="secondLayout descr" colorBg="#e2e2e2" />
-      <Layout title="thirdLayout title" descr="thirdLayout descr" urlBg={background} />
+      <Header title="Pokemon game" descr="This is simple triple triad card game" />
+      <Layout title="firstLayout title" urlBg={background1}>
+        <p>
+          In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid. Each player has five
+          cards in a hand and the aim is to capture the opponent's cards by turning them into the player's own color of red or blue.
+        </p>
+        <p>
+          To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card
+          color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where
+          the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be
+          captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the
+          player's color instead.
+        </p>
+      </Layout>
+      <Layout title="Cards" colorBg="#e2e2e2">
+        <div className="flex">
+          {POKEMONS.map((item) => (
+            <PokemonCard key={item.id} name={item.name} type={item.type} img={item.img} values={item.values} id={item.id} />
+          ))}
+        </div>
+      </Layout>
+      <Layout title="thirdLayout title" urlBg={background2}>
+        <p>
+          In the game two players face off against one another, one side playing as "blue", the other as "red" on a 3x3 grid. Each player has five
+          cards in a hand and the aim is to capture the opponent's cards by turning them into the player's own color of red or blue.
+        </p>
+        <p>
+          To win, a majority of the total ten cards played (including the one card that is not placed on the board) must be of the player's card
+          color. To do this, the player must capture cards by placing a card adjacent to an opponent's card whereupon the 'ranks' of the sides where
+          the two cards touch will be compared. If the rank of the opponent's card is higher than the player's card, the player's card will be
+          captured and turned into the opponent's color. If the player's rank is higher, the opponent's card will be captured and changed into the
+          player's color instead.
+        </p>
+      </Layout>
       <Footer />
     </>
   );
